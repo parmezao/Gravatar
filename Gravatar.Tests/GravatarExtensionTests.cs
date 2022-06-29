580827f0afec3eabd852c0fb48a71a2de6521571
Wagner Parmezão <wagner@parmex.com.br>
2022-06-29 14:07:58 -0300

    Implementado suporte ao tamanho da imagem

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -5,14 +5,16 @@ public class GravatarExtensionTests
 {
     [TestCategory("Gravatar Tests")]
     [TestMethod("Should validate Gravatar extension")]
-    [DataRow(null, false)]
-    [DataRow(" ", false)]
-    [DataRow("a@a", false)]
-    [DataRow("a@parmex.com", false)]
-    [DataRow("wagner@parmex.com.br", true)]
-    public void ShouldValidateGravatar(string email, bool status)
+    [DataRow(null, 200, false)]
+    [DataRow(" ", 200, false)]
+    [DataRow("a@a", 200, false)]
+    [DataRow("a@parmex.com", 200, false)]
+    [DataRow("wagner@parmex.com.br", null, true)]
+    [DataRow("wagner@parmex.com.br", 200, true)]
+    public void ShouldValidateGravatar(string email, int? size, bool status)
     {
-        var result = "https://www.gravatar.com/avatar/203b1d2b1069f155a607905c97f1bb58";
-        Assert.AreEqual((email.ToGravatar() == result), status);
+        var imageSize = size.HasValue ? size.Value.ToString() : "60";
+        var result = $"https://www.gravatar.com/avatar/203b1d2b1069f155a607905c97f1bb58?s={imageSize}";
+        Assert.AreEqual((email.ToGravatar(size ?? 60) == result), status);
     }
 }
